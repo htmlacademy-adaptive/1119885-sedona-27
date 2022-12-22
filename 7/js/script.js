9c96a73b0d864a6c996f63bf8ed3d99ea15e1026
Keks <keks@htmlacademy.ru>
2022-12-22 20:52:31 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/script.js
@@ -0,0 +1,19 @@
+const header = document.querySelector('.header')
+const nav = document.querySelector('.navigation')
+
+header.classList.remove('header--no-js')
+nav.classList.remove('navigation--no-js')
+
+const navButton = document.querySelector('.navigation__menu-button')
+const navList = document.querySelector('.navigation__list')
+
+navButton.addEventListener('click', function () {
+  navList.classList.toggle('navigation__list--opened')
+})
+
+window.addEventListener('click', function (e) {
+  const target = e.target
+  if (!target.closest('.navigation__list') && !target.closest('.navigation__menu-button')) {
+    navList.classList.remove('navigation__list--opened')
+  }
+})
